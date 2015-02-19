b29d57053581ca65f0a5b21a1062ffdadf5eab2e
jhm520 <jhm520@gmail.com>
2015-02-18 18:28:39 -0800

    query[1]

--- topic_john.php
+++ topic_john.php
@@ -231,7 +231,7 @@ l-37.252,37.253c29.758,29.757,70.867,48.162,116.273,48.162c90.814,0,164.436-73.6
 			post_per_load = post_per_page*page_per_load;
 			
 			var query = location.href.split('#');
-			document.write(query);
+			document.write(query[1]);
 			document.cookies = 'anchor=' + query[1];
 		</script>
 		<?php
