435cf5940ee361d7d63dde700da90248040258cc
jhm520 <jhm520@gmail.com>
2015-02-19 01:17:46 -0800

    dfd

--- js/topic.js
+++ js/topic.js
@@ -30,8 +30,10 @@ var main = function() {
 	$('textarea').autosize();
 	
 	//alert(window.location.hash.split('#')[1]);
-	
-	change_page(window.location.hash.split('#')[1], post_per_page);
+	if (window.location.hash != null)
+	{
+		change_page(window.location.hash.split('#')[1], post_per_page);
+	}
 	
 	//Pagiation
 	$('.number').click(function() {
