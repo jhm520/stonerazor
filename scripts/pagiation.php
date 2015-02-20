b24c31078667179495d76d866639dd50e352769b
jhm520 <jhm520@gmail.com>
2015-02-20 13:13:54 -0800

    Added href to older and newer.

--- scripts/pagiation.php
+++ scripts/pagiation.php
@@ -19,10 +19,10 @@ function pagiation($start,$ipp = 15,$num_rows, $loadstart=0, $ppl=10, $john=Fals
 	if ($john){
 		echo "<div class=\"pagiation\">\n";
 		if ($start > 0) echo "<a name=\"".($start-$ipp)."\" class=\"newer active\" href=\"javascript:void();\">&larr; Newer Content</a>\n"; // strint it so it doesn't go negative
-		else echo "<a class=\"newer\">&larr; Newer Content</a>\n";
+		else echo "<a class=\"newer\" href=\"javascript:void();\">&larr; Newer Content</a>\n";
 		
-		if ($num_rows > $start+$ipp) echo "<a name=\"".($start+$ipp)."\" class=\"older active\" href=\"javascript:void();        \">Older Content&rarr;</a>\n";
-		else echo "<a class=\"older\">Older Content &rarr;</a>\n";
+		if ($num_rows > $start+$ipp) echo "<a name=\"".($start+$ipp)."\" class=\"older active\" href=\"javascript:void();\">Older Content&rarr;</a>\n";
+		else echo "<a class=\"older\" href=\"javascript:void();\">Older Content &rarr;</a>\n";
 		
 		echo "<div class=\"numbers\">\n";
 		
